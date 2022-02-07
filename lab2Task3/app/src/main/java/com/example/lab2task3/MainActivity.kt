c7b5c7ef09e9a737791ff7a692d6ee89017483eb
vikonnikova <ikonnikova1995@gmail.com>
2022-02-07 19:01:37 +0300

    fixed homework

--- lab2Task3/app/src/main/java/com/example/lab2task3/MainActivity.kt
+++ lab2Task3/app/src/main/java/com/example/lab2task3/MainActivity.kt
@@ -5,16 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-   fun test (x:Int = 1, y:Int = 3) {
-       if (((x in -4..4) && (y in -3..0)) && ((x in -2..2) && (y in 0..4))
-           && ((x.toDouble() !in -0.5..0.5) && (y.toDouble() !in -0.5..-1.5)))
-               print(true)
-       else print(false)
-   }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        test()
+//    fun test(x: Int = 1, y: Int = 3) {
+        var isInArea: Boolean = false
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+
+
+        }
+
     }
-}

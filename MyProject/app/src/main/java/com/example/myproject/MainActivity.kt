a41962c9562c2abe4f9076cd9949239e7c841e57
vikonnikova <ikonnikova1995@gmail.com>
2022-05-13 20:41:13 +0300

    1

--- MyProject/app/src/main/java/com/example/myproject/MainActivity.kt
+++ MyProject/app/src/main/java/com/example/myproject/MainActivity.kt
@@ -1,65 +1,15 @@
 package com.example.myproject
 
+import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextUtils
-import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.core.text.isDigitsOnly
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var resultTV = findViewById<TextView>(R.id.tvResult)
-        var firstNumber = findViewById<EditText>(R.id.firstNumber)
-        var sign = findViewById<EditText>(R.id.sign)
-        var secondNumber = findViewById<EditText>(R.id.secondNumber)
-        var buttonResult = findViewById<Button>(R.id.button)
-
-        buttonResult.setOnClickListener {
-
-            var enteredNumberOne =
-            if (firstNumber.text.toString().isNotEmpty()) {
-                firstNumber.text.toString().toDouble()
-            } else {
-                0.0
-            }
-
-            var enteredNumberTwo =
-                if (secondNumber.text.toString().isNotEmpty()) {
-                    secondNumber.text.toString().toDouble()
-                } else {
-                    0.0
-                }
-
-            var enteredSign = sign.text.toString()
-
-            when (enteredSign) {
-                "+" -> {
-                    resultTV.text = (enteredNumberOne + enteredNumberTwo).toString()
-                }
-                "-" -> {
-                    resultTV.text = (enteredNumberOne - enteredNumberTwo).toString()
-                }
-                "*" -> {
-                    resultTV.text = (enteredNumberOne * enteredNumberTwo).toString()
-                }
-                "/" -> {
-                    if (enteredNumberTwo !== 0.0) {
-                        resultTV.text = (enteredNumberOne / enteredNumberTwo).toString()
-                    } else {
-                        resultTV.text = "На 0 делить нельзя"
-                    }
-                }
-                else -> resultTV.text = "Неверный знак"
-            }
-        }
     }
 }
 
--- Projects/app/src/main/java/com/example/projects/MainActivity.kt
+++ Projects/app/src/main/java/com/example/projects/MainActivity.kt
@@ -2,46 +2,19 @@ package com.example.projects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.projects.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    private val adapter = BookAdapter()
-    private val imageIdList =
-        listOf(R.drawable.ic_brave_new_world, R.drawable.ic_jane_eyre, R.drawable.ic_little_women)
-    private var imageIndex = 0
-    private val nameList = listOf("Brave New World", "Jane Eyre", "Little Women")
-    private var nameIndex = 0
-    private val descriptionList =
-        listOf("Book one description", "Book two description", "Book three description")
-    private var descriptionIndex = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        init()
-    }
+    override fun onCreate(savedInstanseState: Bundle?) {
+        super.onCreate(savedInstanseState)
+        setContentView(R.layout.activity_main)
 
-    private fun init() {
-        binding.apply {
-            rcView.layoutManager = LinearLayoutManager(this@MainActivity)
-            rcView.adapter = adapter
-            buttonAddBook.setOnClickListener {
-                if (imageIndex > 2) imageIndex = 0
-                if (nameIndex > 2) nameIndex = 0
-                if (descriptionIndex > 2) descriptionIndex = 0
-                val book = Book(
-                    imageIdList[imageIndex],
-                    nameList[nameIndex],
-                    descriptionList[descriptionIndex]
-                )
-                adapter.addBook(book)
-                imageIndex++
-                nameIndex++
-                descriptionIndex++
-            }
+        if (savedInstanseState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, MainFragment(), "fragment_tag")
+                .commitNow()
         }
     }
 }
